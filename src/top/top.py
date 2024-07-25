c00cf03e8b6711f2efc87d00c2fa05e64b7057ed
matsu.jp <100035809+matsujp@users.noreply.github.com>
2024-07-25 15:35:50 +0900

    Update top.py

--- src/top/top.py
+++ src/top/top.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 
 def page():
-    st.title(":sparkles: Welcome to :blue[SNS Analytics](仮)")
+    st.title(":sparkles: Welcome to :blue[Social Vision]")
     st.divider()
     st.text("ここにサービスの説明を記載予定")
